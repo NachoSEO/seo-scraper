609b8c6df5e9481653280fc0bc26d1a3e8a4adb1
nacho <nacho.mascort@softonic.com>
2021-08-06 20:48:33 +0200

    quick fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 const { scraperService } = require('./providers/providers')
 
-// module.exports = scraperService;
-
-scraperService.scrape({ url: 'https://github.com/NachoSEO/seo-scraper' })
-  .then(elements => console.log(elements))
+module.exports = scraperService;
